e73182db3eab3b6eae2f27a8cb11e98b33ebaa7d
David Sanchez (MSFT) <david.sanchez@microsoft.com>
2019-03-16 00:10:02 -0700

    Update AppSettings.cs

--- Source/SmartHotel.Clients/SmartHotel.Clients/AppSettings.cs
+++ Source/SmartHotel.Clients/SmartHotel.Clients/AppSettings.cs
@@ -21,7 +21,7 @@ namespace SmartHotel.Clients.Core
 
         // Endpoints
         const string defaultImagesBaseUri = "https://sh360imgdev.blob.core.windows.net";
-        const string defaultRoomDevicesEndpoint = "http://13.66.132.178:2000/api";
+        const string defaultRoomDevicesEndpoint = "http://13.77.150.201:2000/api";
 
         // Maps
         const string defaultBingMapsApiKey = "AkSuJ-YtW4VDvIzErxK3ke2ILQD1muWwS2KN2QvhqHobx4YBEIYqkEVBLyx1LYby";
@@ -40,7 +40,7 @@ namespace SmartHotel.Clients.Core
         const bool defaultHasBooking = false;
 
 		// Room Devices
-	    const string defaultRoomId = "4dfa6bc0-8a78-4f9b-97ce-6414ff723ff8";
+	    const string defaultRoomId = "ab847902-3ec4-4888-8ca8-0752796c89e4";
 
         // Fakes
         const bool defaultUseFakes = false;
